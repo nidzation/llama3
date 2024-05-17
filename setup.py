d704658c1dcffead6f0bbc2be491445a1c83625b
nidzation <nikolahd96@gmail.com>
2024-05-17 16:18:32 +0200

    mn

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed in accordance with the terms of the Llama 3 Community License Agreement.
 
-from setuptools import find_packages, setup
+from setuptools import find_packages, setup # type: ignore
 
 
 def get_requirements(path: str):
